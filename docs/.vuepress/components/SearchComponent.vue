ddb34148eef5218bc4ecbe8f1695e35309d52fd5
wangyou2 <2712334479@qq.com>
2023-11-15 16:08:49 +0800

    修改主题

---
+++ docs/.vuepress/components/SearchComponent.vue
@@ -0,0 +1,133 @@
+<!-- SearchComponent.vue -->
+<template>
+    <div>
+      <input
+        v-model="searchQuery"
+        @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @keydown.enter="handleEnter"
+        @keydown.arrow-up.prevent="handleArrowUp"
+        @keydown.arrow-down.prevent="handleArrowDown"
+        placeholder="Search..."
+        class="card-search-input"
+      />
+      <ul v-if="isFocused && filteredData.length > 0">
+        <li
+          v-for="(card, index) in filteredData"
+          :key="index"
+          :class="{ activeselect: index === selectedIndex }"
+          @mousedown.prevent="handleItemClick(index)"
+        >
+          <a :href="card.cardSrc" target="_blank">
+            <!-- <img :src="card.cardImgSrc" alt="Card Image" class="card-search-img"/> -->
+            <div>
+              <h3 class="card-search-h">{{ card.cardName }}</h3>
+              <p>{{ card.cardContent }}</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <!-- 修改此处的条件 -->
+    <!-- <p v-else-if="isFocused && searchQuery !== ''">No matching results.</p> -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      cardData: {
+        type: Array,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        searchQuery: "",
+        isFocused: false,
+        selectedIndex: -1,
+      };
+    },
+    computed: {
+      filteredData() {
+        const query = this.searchQuery.toLowerCase();
+        // 检查是否有 cardData 并确保它是一个数组
+        if (this.cardData && Array.isArray(this.cardData) && query !== '') {
+          return this.cardData.filter((card) => {
+            return (
+              card.cardName.toLowerCase().includes(query) ||
+              card.cardContent.toLowerCase().includes(query)
+            );
+          });
+        } else {
+          return [];
+        }
+      },
+    },
+    methods: {
+        handleEnter() {
+        const selectedCard = this.filteredData[this.selectedIndex];
+        if (selectedCard) {
+          window.open(selectedCard.cardSrc, "_blank");
+        }
+      },
+      handleInput() {
+        this.selectedIndex = -1; // Reset selected index on input change
+      },
+      handleFocus() {
+        this.isFocused = true;
+      },
+      handleBlur() {
+        this.isFocused = false;
+      },
+      handleItemClick(index) {
+        const selectedCard = this.filteredData[index];
+        if (selectedCard) {
+          window.open(selectedCard.cardSrc, "_blank");
+        }
+      },
+      
+      handleArrowUp() {
+      if (this.selectedIndex > 0) {
+        this.selectedIndex--;
+      }
+    },
+    handleArrowDown() {
+      if (this.selectedIndex < this.filteredData.length - 1) {
+        this.selectedIndex++;
+      }
+    },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    cursor: pointer;
+    margin-bottom: 10px;
+  }
+  
+  .activeselect {
+    background-color: #eee;
+  }
+  .card-search-img{
+    float:left;
+    height: 50px;
+    margin-right: 10px;
+  }
+  .card-search-h{
+    margin: 0;
+  }
+  .card-search-input{
+    border: 1px solid #ccc;
+    border-radius: 15px;
+    height: 30px;
+    padding: 0 15px;
+  }
+  </style>
+  
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,6 +7,7 @@ import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
 import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
+import { readFileList, readTotalFileWords, readEachFileWords } from './webSiteInfo/readFile';// 站点信息
 
 const DOMAIN_NAME = 'wangyou.ink' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
@@ -18,7 +19,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: "Kronos's blog",
+      title: "Kronos' blog",
       description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
     }
   },
@@ -29,6 +30,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 导航配置
     nav: [
       { text: '首页', link: '/' },
+      { text: '导航站', link: '/tools/' },
       {
         text: '前端',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
@@ -47,7 +49,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
               { text: '《JavaScript高级程序设计》', link: '/note/js/' },
               { text: '《ES6 教程》', link: '/note/es6/' },
               { text: '《Vue》', link: '/note/vue/' },
-              { text: '《React》', link: '/note/react/' },
               {
                 text: '《TypeScript 从零实现 axios》',
                 link: '/note/typescript-axios/',
@@ -117,9 +118,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
     ],
+    
+
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'jywangyou/notes-blog', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -172,12 +175,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     blogger: {
       avatar: 'https://cdn.statically.io/gh/jywangyou/picx-images-hosting@master/xiaohei.5g9w47ni9lo0.webp',
       name: 'Kronos',
-      slogan: '星空下仰望巨人',
+      slogan: '纵有疾风起，人生不言弃',
     },
 
     // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
     social: {
-      // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
+      iconfontCssFile: '//at.alicdn.com/t/c/font_4330037_9cegdl3g03m.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
       icons: [
         {
           iconClass: 'icon-youjian',
@@ -190,8 +193,18 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           link: 'https://github.com/jywangyou',
         },
         {
-          iconClass: 'icon-erji',
-          title: '听音乐',
+          iconClass: 'icon-QQ',
+          title: 'QQ',
+          link: 'https://qm.qq.com/q/OeKpLeEyWG',
+        },
+        {
+          iconClass: 'icon-douban',
+          title: '豆瓣',
+          link: 'https://www.douban.com/people/wangyous/',
+        },
+        {
+          iconClass: 'icon-wangyiyun1',
+          title: '网易云',
           link: 'https://music.163.com/#/playlist?id=5309180789',
         },
       ],
@@ -213,12 +226,47 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     },
 
     // 自定义hmtl(广告)模块
-    htmlModules
+    htmlModules,
+    // 站点配置（首页 & 文章页）
+    blogInfo: {
+      blogCreate: '2023-11-11', // 博客创建时间
+      indexView: true,  // 开启首页的访问量和排名统计，默认 true（开启）
+      pageView: true,  // 开启文章页的浏览量统计，默认 true（开启）
+      readingTime: true,  // 开启文章页的预计阅读时间，条件：开启 eachFileWords，默认 true（开启）。可在 eachFileWords 的 readEachFileWords 的第二个和第三个参数自定义，默认 1 分钟 300 中文、160 英文
+      eachFileWords: readEachFileWords([''], 300, 160),  // 开启每个文章页的字数。readEachFileWords(['xx']) 关闭 xx 目录（可多个，可不传参数）下的文章页字数和阅读时长，后面两个参数分别是 1 分钟里能阅读的中文字数和英文字数。无默认值。readEachFileWords() 方法默认排除了 article 为 false 的文章
+      mdFileCountType: 'archives',  // 开启文档数。1. archives 获取归档的文档数（默认）。2. 数组 readFileList(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文档数。提示：readFileList() 获取 docs 下所有的 md 文档（除了 `.vuepress` 和 `@pages` 目录下的文档）
+      totalWords: 'archives',  // 开启本站文档总字数。1. archives 获取归档的文档数（使用 archives 条件：传入 eachFileWords，否则报错）。2. readTotalFileWords(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文章字数。无默认值
+      moutedEvent: '.tags-wrapper',   // 首页的站点模块挂载在某个元素后面（支持多种选择器），指的是挂载在哪个兄弟元素的后面，默认是热门标签 '.tags-wrapper' 下面，提示：'.categories-wrapper' 会挂载在文章分类下面。'.blogger-wrapper' 会挂载在博客头像模块下面
+      // 下面两个选项：第一次获取访问量失败后的迭代时间
+      indexIteration: 2500,   // 如果首页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 10 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+      pageIteration: 2500    // 如果文章页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 10 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+      // 说明：成功获取一次访问量，访问量 + 1，所以第一次获取失败后，设置的每个隔段重新获取时间，将会影响访问量的次数。如 100 可能每次获取访问量 + 3
+    },
+    // 背景大图
+    indexImg: {
+      navColor: 2,    // 导航栏左侧名字、中间搜索框、右侧字体的颜色，1 是黑色，2 是白色。默认是 1
+      switchNavColor: true,    // 页面移出大图片的位置后，navColor 是否变换，如由白色变黑色，黑色变白色。默认是 false
+      // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
+      bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
+      bgTimeColorArray: ['transparent', 'rgba(255, 148, 48, .2)', 'rgba(0, 0, 0, .3)', 'rgba(0, 0, 0, .5)'],   // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效。提示：如果不想要这个效果，但是又想要时间窗口提示效果，则改为 ['transparent', 'transparent', 'transparent', 'transparent']
+      descFade: true,   // 是否开启图片中间描述的淡入效果，默认为 false
+      desc: ["Web前端技术博客，积跬步以至千里，致敬每个爱学习的你 —— 来自 Evan Xu", "愿你能成为自己的英雄，不惧离别，不怕孤独 —— 来自 Kronos", "心中有世界，闭目自可观","时间在走，没有回头，只有路口"],  // 多个描述，如果填写则覆盖 config.js 的 description，不填写默认读取 config.js 的 description，descFade 为 true 生效
+      descFontSize: '1.4rem',   // desc 的字体大小，默认 1.4rem。提示：原主题是 1.1rem
+      descFadeInTime: 200,  // 描述的淡入效果持续时间，descFade 为 true 生效，默认 200 毫秒
+      descFadeOutTime: 100,  // 描述的淡出效果持续时间，descFade 为 true 生效，默认 100 毫秒
+      descNextTime: 800,  // 当存在多个 desc 时，一个 desc 展示完后或准备开始时，多少秒后出现下一个 desc，默认 800 毫秒
+      bubble: false,    // 是否开启图片的气泡效果，默认为 false
+      bubblePosition: 0,  // 气泡效果的位置，范围：0-100，不同数值代表不同的起始位置，0是整个图片，50是半张图（一半的下方）。bubble 为 true 生效。默认是 0
+      bubbleNum: 200,   // 气泡的个数，bubble 为 true 生效，默认 200 个
+    },
+    
   },
 
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
-    ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/font_3114978_qe0b39no76.css' }],//代码块折叠的箭头图标
+    ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],//解决Chrome 统计访问不准确问题
+    ['link', { rel: 'stylesheet', href: 'https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css' }],//站点信息相关图标
+    ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/font_3114978_qe0b39no76.css' }],//代码块折叠的箭头图标&时间提示的图标
     ['script', { src: 'https://cdn.staticfile.org/twikoo/1.6.7/twikoo.all.min.js' }],//评论系统twikoo
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     [
@@ -370,8 +418,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     [
     	{
         	name: 'custom-plugins',
-        	globalUIComponents: ["Twikoo","BlockToggle"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
+        	globalUIComponents: ["Twikoo","BlockToggle","GlobalTip","PageInfo","LastReadingPopup","Fantasy"] // 这里分别是评论组件，代码块组件，时间提示组件,站点信息组件,记录阅读位置组件,全局背景图组件
     	}
+    ],
+    [
+      'reading-progress' //这个是页面阅读进度进度条（顶部插件）
     ],
     // [
     // 	{
