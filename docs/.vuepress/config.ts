943a0ce0c13854cb9de474f998576ed04b47ba37
wangyou2 <2712334479@qq.com>
2023-12-21 15:21:38 +0800

    增加百度统计

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,6 +8,7 @@ import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 import { readFileList, readTotalFileWords, readEachFileWords } from './webSiteInfo/readFile';// 站点信息
+import baidutj from './common/baidutj';//百度统计
 
 const DOMAIN_NAME = 'wangyou.ink' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
@@ -302,6 +303,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ['script', {}, baidutj],//百度统计
     // [
     //   'script',
     //   {
