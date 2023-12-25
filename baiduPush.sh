7f3cf5ce7e471869f8212b149488d26600d9c14a
jywangyou <2712334479@qq.com>
2023-12-25 16:33:14 +0800

    更新百度推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://wangyou.ink&token=mzeT25J6TVlVFWZw"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.wangyou.ink&token=mzeT25J6TVlVFWZw"
 
 rm -rf urls.txt # 删除文件
