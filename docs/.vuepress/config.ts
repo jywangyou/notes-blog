774bccf2b770caccc382fec5410193fe910de79e
jywangyou <2712334479@qq.com>
2024-01-10 16:43:42 +0800

    更新内容

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -55,7 +55,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           {
             text: '学习笔记',
             items: [
-              { text: '《JavaScript教程》', link: '/note/javascript/' },
+              { text: '《JavaScript》', link: '/note/javascript/' },
               // { text: '《JavaScript高级程序设计》', link: '/note/js/' },
               { text: '《ES6 教程》', link: '/note/es6/' },
               { text: '《Vue》', link: '/note/vue/' },
